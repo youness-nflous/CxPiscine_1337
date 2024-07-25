c331537cdd14893a4af347e29330653c1f2adb1f
youness-nflous <anayouness51@gmail.com>
2024-07-25 22:12:43 +0100

    1337

---
+++ c02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,26 @@
+int	ft_str_is_lowercase(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+// Not include the "main" it's just for the test.
+#include <stdio.h>
+
+int main()
+{
+	char str[] = "hello:)";
+
+	printf("%d", ft_str_is_lowercase(str));
+
+	return (0);
+}
