f91eddc958add847ec3e9af623a07943ec111dd4
youness-nflous <anayouness51@gmail.com>
2024-07-29 18:00:42 +0100

    1337

---
+++ c03/ex01/ft_strncmp.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int main()
+{
+	char s1[] = "hola";
+	char s2[] = "hol";
+	unsigned int n;
+
+	n = 4;
+
+	printf("%d", ft_strncmp(s1, s2, n));
+	return (0);
+}
