4d6dfb05b6350bbfb1bdbb3c16326bca086548f4
youness-nflous <anayouness51@gmail.com>
2024-07-30 21:42:49 +0100

    1337

---
+++ c03/ex02/ft_strcat.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+int main()
+{
+	char src[] = "Hello";
+	char dest[10] = {};
+
+	ft_strcat(dest, src);
+
+	ft_putstr(dest);
+
+	return (0);	
+}
