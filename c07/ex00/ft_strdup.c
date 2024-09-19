fd574db9009c54f9a9ae7def9ff779ecbc561166
youness-nflous <anayouness51@gmail.com>
2024-09-19 22:12:03 +0100

    1337

--- c07/ex00/ft_strdup.c
+++ c07/ex00/ft_strdup.c
@@ -1,43 +1,38 @@
 #include <stdlib.h>
 
-char	*ft_strdup(char *src)
+char	*ft_strdup(const char *str)
 {
-	int i;
-	char *dup;
+	char *ptr;
+	size_t len;
 
-	i = 0;
-	while (src[i])
-		i++;
+	len = 0;
 
-	dup = malloc(sizeof(char) * (i + 1));
+	while (str[len])
+	{
+		len++;
+	}
 
-	if (dup == NULL)
+	ptr = malloc(sizeof(char) * (len + 1));
+
+	if (ptr == NULL)
 		return (NULL);
-
-	i = 0;
-	while (src[i])
-	{
-		dup[i] = src[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
-}
-
-#include <stdio.h>
-int main()
-{
-	char str[] = "youness_anflous";
-
-	char *dup_str = ft_strdup(str);
 	
-	if (dup_str != NULL)
+	len = 0;
+	while (str[len])
 	{
-		printf("%s", dup_str);
-		free(dup_str);
-	} else 
-	{
-		printf("Memory allocation failed.");
+		ptr[len] = str[len];
+		len++;
 	}
-	return (0);
+	ptr[len] = '\0';
+	return (ptr);
 }
+
+//int main()
+//{
+//	char src[] = "youness_anflous";
+//	char *dup;
+//	dup = ft_strdup(src);
+//	printf("%s", dup);
+//	free(dup);
+//	return (0);
+//}
